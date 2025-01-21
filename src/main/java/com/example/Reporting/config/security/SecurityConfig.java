6609fb53cb80276ac14efaa07dc213cc4863a9c9
MarkBorecki <marek.borecki88@gmail.com>
2025-01-21 19:46:53 +0200

    remove redundant code from SecurityConfig

--- src/main/java/com/example/Reporting/config/security/SecurityConfig.java
+++ src/main/java/com/example/Reporting/config/security/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.example.Reporting.config.security;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -39,11 +37,6 @@ public class SecurityConfig {
                 )
                 .addFilterAfter(oncePerRequestFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    @Bean
-    public AuthenticationManager noAuthenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
 
     @Bean
